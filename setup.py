25c125cd51f4ea844b8bf405c9c84fde150559f9
mrstephenneal <stephen.neal14@gmail.com>
2018-07-17 12:15:56 -0400

    PySimpleGUI added as a dependency.

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='dirutility',
-    version='0.2.13',
+    version='0.2.14',
     packages=find_packages(),
     install_requires=[
         'tqdm',
         'looptools',
         'databasetools',
+        'PySimpleGUI'
     ],
     url='https://github.com/mrstephenneal/dirutility',
     license='MIT License',
     author='Stephen Neal',
     author_email='stephen@stephenneal.net',
-    description='GUI window added to walk.py, accessible when walk.py is called as main script.',
+    description='PySimpleGUI added as a dependency.',
     long_description='Utility functions for quickly reading directory contents and refactoring folder structure'
 )
