bd7223c78905542f1c3dd36caa67d1e91d683a29
mrstephenneal <stephen.neal14@gmail.com>
2019-02-21 13:32:52 -0500

    ADD FTP to dirutility modules

--- dirutility/__init__.py
+++ dirutility/__init__.py
@@ -1,7 +1,8 @@
 from dirutility.move import FlattenTree, CreateTree, move_files_to_folders
 from dirutility.walk import DirPaths, DirTree
 from dirutility.desktop import desktop
+from dirutility.ftp import FTP
 from dirutility.backup import ZipBackup
 
 
-__all__ = ['FlattenTree', 'CreateTree', 'move_files_to_folders', 'DirTree', 'DirPaths', 'desktop', 'ZipBackup']
+__all__ = ['FlattenTree', 'CreateTree', 'move_files_to_folders', 'DirTree', 'DirPaths', 'desktop', 'ZipBackup', 'FTP']
