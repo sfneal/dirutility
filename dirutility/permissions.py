6fd8bad91581e8d1b3f6179222aef0f522bf83b0
mrstephenneal <stephen.neal14@gmail.com>
2019-03-04 13:02:00 -0500

    OPTIMIZE permissions module to house related methods in a class

--- dirutility/permissions.py
+++ dirutility/permissions.py
@@ -29,67 +29,7 @@ PERMISSIONS = {
 }
 
 
-def get_permissions_mode(permission_octal, name):
-    """Retrieve a user permissions bitwise code."""
-    read = PERMISSIONS[name]['read']
-    write = PERMISSIONS[name]['write']
-    execute = PERMISSIONS[name]['execute']
-
-    # Read
-    if permission_octal == 4:
-        return read & ~write & ~execute
-
-    # Write
-    elif permission_octal == 2:
-        return ~read & write & ~execute
-
-    # Execute
-    elif permission_octal == 1:
-        return ~read & ~write & execute
-
-    # Read & Write
-    elif permission_octal == 6:
-        return read & write & ~execute
-
-    # Read & Execute
-    elif permission_octal == 5:
-        return read & ~write & execute
-
-    # Write & Execute
-    elif permission_octal == 3:
-        return ~read & write & execute
-
-    # Read, Write & Execute
-    elif permission_octal == 7:
-        return read & write & execute
-
-    # No read, write or execute by default
-    else:
-        return ~read & ~write & ~execute
-
-
-def get_permissions_octal(file_path):
-    """Return file permissions in absolute notation (octal) format."""
-    return stat.S_IMODE(os.lstat(file_path).st_mode)
-
-
-def set_permissions_mode_from_octal(file_path, code):
-    """
-    Set permissions for a file or directory.
-
-    :param file_path: Path to a file or directory
-    :param code: Permission code in absolute notation (octal)
-    :return:
-    """
-    # Unpack permissions tuple
-    user, group, other = tuple(str(code[-3:])) if len(str(code)) > 3 else tuple(str(code))
-    user, group, other = int(user), int(group), int(other)
-    mode = get_permissions_mode(user,
-                                'user') & get_permissions_mode(group, 'group') & get_permissions_mode(other, 'other')
-    os.chmod(file_path, mode)
-
-
-def octal_permissions_mode(user=(False, False, False), group=(False, False, False), other=(False, False, False)):
+def make_octal_permissions_mode(user=(False, False, False), group=(False, False, False), other=(False, False, False)):
     """
     Create a permissions bit in absolute notation (octal).
 
@@ -139,34 +79,82 @@ def octal_permissions_mode(user=(False, False, False), group=(False, False, Fals
     return int(mode)
 
 
-def add_read_permissions(path):
-    """Add read permissions from this path, while keeping all other permissions intact."""
-    reading = PERMISSIONS['user']['read'] + PERMISSIONS['group']['read'] + PERMISSIONS['other']['read']
-    os.chmod(path, reading)
+def set_permissions_mode_from_octal(file_path, code):
+    """
+    Set permissions for a file or directory.
+
+    :param file_path: Path to a file or directory
+    :param code: Permission code in absolute notation (octal)
+    :return:
+    """
+    # Unpack permissions tuple
+    user, group, other = tuple(str(code[-3:])) if len(str(code)) > 3 else tuple(str(code))
+    user, group, other = int(user), int(group), int(other)
+    mode = get_permissions_mode(user,
+                                'user') & get_permissions_mode(group, 'group') & get_permissions_mode(other, 'other')
+    os.chmod(file_path, mode)
 
 
-def add_write_permissions(path):
-    """Add write permissions from this path, while keeping all other permissions intact."""
-    writing = PERMISSIONS['user']['write'] + PERMISSIONS['group']['write'] + PERMISSIONS['other']['write']
-    os.chmod(path, writing)
+def get_permissions_mode(permission_octal, name):
+    """Retrieve a user name group permissions bitwise code."""
+    read = PERMISSIONS[name]['read']
+    write = PERMISSIONS[name]['write']
+    execute = PERMISSIONS[name]['execute']
+
+    # Read
+    if permission_octal == 4:
+        return read & ~write & ~execute
+
+    # Write
+    elif permission_octal == 2:
+        return ~read & write & ~execute
+
+    # Execute
+    elif permission_octal == 1:
+        return ~read & ~write & execute
+
+    # Read & Write
+    elif permission_octal == 6:
+        return read & write & ~execute
+
+    # Read & Execute
+    elif permission_octal == 5:
+        return read & ~write & execute
+
+    # Write & Execute
+    elif permission_octal == 3:
+        return ~read & write & execute
+
+    # Read, Write & Execute
+    elif permission_octal == 7:
+        return read & write & execute
+
+    # No read, write or execute by default
+    else:
+        return ~read & ~write & ~execute
 
 
-def no_access_permissions(path):
-    """Add read permissions from this path, while keeping all other permissions intact."""
-    reading = PERMISSIONS['user']['execute'] + PERMISSIONS['group']['execute'] + PERMISSIONS['other']['execute']
-    os.chmod(path, reading)
+class Permissions:
+    def __init__(self, file_path):
+        self.file_path = file_path
 
+    @property
+    def octal(self):
+        """Return file permissions in absolute notation (octal) format."""
+        return stat.S_IMODE(os.lstat(self.file_path).st_mode)
 
-def add_rwe_user(path):
-    """Add read, write & execute permissions for owner user."""
-    os.chmod(path, PERMISSIONS['user']['all'])
+    def allow(self, privilege):
+        """Add an allowed privilege (read, write, execute, all)."""
+        assert privilege in PERMISSIONS['user'].keys()
+        reading = PERMISSIONS['user'][privilege] + PERMISSIONS['group'][privilege] + PERMISSIONS['other'][privilege]
+        os.chmod(self.file_path, reading)
 
+    def allow_rwe(self, name):
+        """Allow all privileges for a particular name group (user, group, other)."""
+        assert name in PERMISSIONS.keys()
+        os.chmod(self.file_path, PERMISSIONS[name]['all'])
 
-def add_rwe_group(path):
-    """Add read, write & execute permissions for group users."""
-    os.chmod(path, PERMISSIONS['group']['all'])
-
-
-def add_rwe_other(path):
-    """Add read, write & execute permissions for all users."""
-    os.chmod(path, PERMISSIONS['other']['all'])
+    def revoke_access(self):
+        """Revoke all access to this path."""
+        reading = PERMISSIONS['user']['execute'] + PERMISSIONS['group']['execute'] + PERMISSIONS['other']['execute']
+        os.chmod(self.file_path, reading)
