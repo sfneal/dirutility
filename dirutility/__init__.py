5c979f415aa1b047398ee8820d5ba932d1d5a6ce
mrstephenneal <stephen.neal14@gmail.com>
2019-03-04 13:02:54 -0500

    ADD Permissions class to dirutility __all__ declaration

--- dirutility/__init__.py
+++ dirutility/__init__.py
@@ -1,9 +1,10 @@
 from dirutility.move import FlattenTree, CreateTree, move_files_to_folders
 from dirutility.walk import DirPaths, DirTree
 from dirutility.view import desktop, open_window
-from dirutility.ftp import FTP
 from dirutility.backup import ZipBackup
+from dirutility.ftp import FTP
+from dirutility.permissions import Permissions
 
 
 __all__ = ['FlattenTree', 'CreateTree', 'move_files_to_folders', 'DirTree', 'DirPaths', 'desktop', 'ZipBackup', 'FTP',
-           'open_window']
+           'open_window', 'Permissions']
