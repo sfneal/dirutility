b839cd6566a912989ec3b451241a4df4fdb0d679
mrstephenneal <stephen.neal14@gmail.com>
2019-04-18 10:19:45 -0400

    ADD multiprocess class test
    BUMP looptools dependency.

--- tests/test_multiprocess.py
+++ tests/test_multiprocess.py
@@ -3,7 +3,7 @@ from time import sleep
 
 from looptools import Timer
 
-from dirutility.multiprocess import pool_process
+from dirutility.multiprocess import pool_process, PoolProcess
 
 
 def test_func(a):
@@ -25,12 +25,37 @@ class TestMultiprocessFunc(unittest.TestCase):
             self.assertEqual(set(self.result), EXPECTED)
 
     @Timer.decorator
-    def test_basic(self):
+    def test_map(self):
         self.result = pool_process(FUNC, ITER)
 
     @Timer.decorator
-    def test_return_vals(self):
+    def test_return(self):
         self.result = pool_process(FUNC, ITER, return_vals=True)
+
+    @Timer.decorator
+    def test_progress(self):
+        self.result = pool_process(FUNC, ITER, return_vals=True, progress_bar=True)
+
+
+class TestMultiprocessClass(unittest.TestCase):
+    def setUp(self):
+        self.result = None
+
+    def tearDown(self):
+        if isinstance(self.result, list):
+            self.assertEqual(set(self.result), EXPECTED)
+
+    @Timer.decorator
+    def test_map(self):
+        self.result = PoolProcess(FUNC, ITER).map()
+
+    @Timer.decorator
+    def test_return(self):
+        self.result = PoolProcess(FUNC, ITER).map_return()
+
+    @Timer.decorator
+    def test_progress(self):
+        self.result = PoolProcess(FUNC, ITER).map_tqdm()
 
 
 if __name__ == '__main__':
