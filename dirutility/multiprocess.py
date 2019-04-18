77b2f060f11a118b91ea7ff4cca0c001574fc1a6
mrstephenneal <stephen.neal14@gmail.com>
2019-04-18 10:03:47 -0400

    ADD progress bar parameter to pool_process func and add test suite

--- dirutility/multiprocess.py
+++ dirutility/multiprocess.py
@@ -1,8 +1,9 @@
 from multiprocessing import cpu_count
 from multiprocessing.pool import Pool
+from tqdm import tqdm
 
 
-def pool_process(func, iterable, cpus=cpu_count(), return_vals=False, cpu_reduction=0):
+def pool_process(func, iterable, cpus=cpu_count(), return_vals=False, cpu_reduction=0, progress_bar=False):
     """
     Multiprocessing helper function for performing looped operation using multiple processors.
 
@@ -11,13 +12,27 @@ def pool_process(func, iterable, cpus=cpu_count(), return_vals=False, cpu_reduct
     :param cpus: Number of cpu cores, defaults to system's cpu count
     :param return_vals: Bool, returns output values when True
     :param cpu_reduction: Number of cpu core's to not use
+    :param progress_bar: Display text based progress bar
     :return:
     """
     with Pool(cpus - abs(cpu_reduction)) as pool:
+        # Return values returned by 'func'
         if return_vals:
-            vals = pool.map(func, iterable)
+            # Show progress bar
+            if progress_bar:
+                vals = [v for v in tqdm(pool.imap_unordered(func, iterable), total=len(iterable))]
+
+            # No progress bar
+            else:
+                vals = pool.map(func, iterable)
+
+            # Close pool and return values
             pool.close()
+            # pool.join()
             return vals
+
+        # Don't capture values returned by 'func'
         else:
             pool.map(func, iterable)
             pool.close()
+            return True
---
+++ tests/test_multiprocess.py
@@ -0,0 +1,37 @@
+import unittest
+from time import sleep
+
+from looptools import Timer
+
+from dirutility.multiprocess import pool_process
+
+
+def test_func(a):
+    sleep(.1)
+    return a * a
+
+
+FUNC = test_func
+ITER = list(range(100))
+EXPECTED = set([a * a for a in range(100)])
+
+
+class TestMultiprocess(unittest.TestCase):
+    def setUp(self):
+        self.result = None
+
+    def tearDown(self):
+        if isinstance(self.result, list):
+            self.assertEqual(set(self.result), EXPECTED)
+
+    @Timer.decorator
+    def test_basic(self):
+        self.result = pool_process(FUNC, ITER)
+
+    @Timer.decorator
+    def test_return_vals(self):
+        self.result = pool_process(FUNC, ITER, return_vals=True)
+
+
+if __name__ == '__main__':
+    unittest.main()
