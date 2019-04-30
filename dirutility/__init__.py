a4f538aac88b2697fa70984a833ee02bc1a6f10f
mrstephenneal <stephen.neal14@gmail.com>
2019-04-30 09:52:06 -0400

    ADD PoolProcess to module level imports

--- dirutility/__init__.py
+++ dirutility/__init__.py
@@ -1,11 +1,11 @@
 from dirutility.backup import ZipBackup
 from dirutility.ftp import FTP
 from dirutility.move import FlattenTree, CreateTree, move_files_to_folders
-from dirutility.multiprocess import pool_process
+from dirutility.multiprocess import pool_process, PoolProcess
 from dirutility.permissions import Permissions
 from dirutility.system import SystemCommand
 from dirutility.view import desktop, open_window
 from dirutility.walk import DirPaths, DirTree
 
 __all__ = ['FlattenTree', 'CreateTree', 'move_files_to_folders', 'DirTree', 'DirPaths', 'desktop', 'ZipBackup', 'FTP',
-           'open_window', 'Permissions', 'pool_process', 'SystemCommand']
+           'open_window', 'Permissions', 'pool_process', 'SystemCommand', 'PoolProcess']
